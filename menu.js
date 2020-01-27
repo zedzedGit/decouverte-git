8e10f366d15caf652b4c03b8f9d21d8c2db1a574
Nicolas <nperigoi@gmail.com>
2020-01-27 11:20:59 +0100

    ajout contenu menu

---
+++ menu.js
@@ -0,0 +1,8 @@
+const menu = `
+    <ul>
+        <li><a href="index.html">Accueil</a></li>
+        <li><a href="contact.html">Contact</a></li>
+    </ul>
+`;
+
+document.getElementById('navigation').innerHTML=menu;
