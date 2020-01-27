de1b41fd37750455aff708bb6e2a7d2cc27ba20a
Nicolas <nperigoi@gmail.com>
2020-01-27 11:57:35 +0100

    ajout page presentation.html

--- menu.js
+++ menu.js
@@ -2,6 +2,7 @@ const menu = `
     <ul>
         <li><a href="index.html">Accueil</a></li>
         <li><a href="contact.html">Contact</a></li>
+        <li><a href="presentation.html">Presentation</a></li>
     </ul>
 `;
 
